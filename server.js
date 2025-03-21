cac457bde9d0f497b36c042eae43f8f983fd01b6
spangheri <spangheri@gmail.com>
2025-03-21 17:18:03 -0300

    Ajustando porta para deploy no Render

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware para permitir requisições de qualquer origem (CORS)
 app.use((req, res, next) => {
